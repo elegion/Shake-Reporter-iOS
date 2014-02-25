999ea2e09d9b0f8c701d1d8d3a0e7c1db193e696
Yuri Buyanov <yuri.buyanov@gmail.com>
2014-02-25 22:24:43 +0400

    project fix

--- ELFeedback/ELFeedbackManager.h
+++ ELFeedback/ELFeedbackManager.h
@@ -6,7 +6,6 @@
 //  Copyright (c) 2013 e-legion. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
 #import "ELFeedbackHTTPProtocol.h"
 #import "ELFeedbackDataProvider.h"
 
--- ELFeedback/UIView+ELSnapshot.h
+++ ELFeedback/UIView+ELSnapshot.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2013 e-legion. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @interface UIView (ELSnapshot)
 
 - (UIImage *)ELSnapshot;
